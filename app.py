6c803b7568a6945a6cb29b368696d34b0c34d571
Alex Tessaract <87167056+AlexTessaract@users.noreply.github.com>
2022-02-01 12:44:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ app = Flask(__name__)
 # Check https://keras.io/applications/
 # or https://www.tensorflow.org/api_docs/python/tf/keras/applications
 
-from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
+from tensorflow.keras.applications.vgg16 import VGG16
 model = MobileNetV2(weights='imagenet')
 
 print('Model loaded. Check http://127.0.0.1:5000/')
